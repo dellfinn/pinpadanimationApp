192919eaace199b557044398a3973e4fe385e93b
Italo Ruan Santos de Brito <paradoxonitalo@gmail.com>
2021-12-19 12:42:46 -0300

    Initial commit

---
+++ lib/mpin/mpin_page.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+import 'mpin_widget.dart';
+
+class MPinPage extends StatefulWidget {
+  @override
+  _MPinPageState createState() => _MPinPageState();
+}
+
+class _MPinPageState extends State<MPinPage> {
+  MPinController mPinController = MPinController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+              colors: [Colors.blue, Colors.purple, Colors.pink],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            )),
+          ),
+          SafeArea(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  MPinWidget(
+                    pinLegth: 5,
+                    controller: mPinController,
+                    onCompleted: (mPin) {
+                      print('Você entrou no MPin  $mPin');
+                      if (mPin == '12345') {
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: Text('Sucesso'),
+                                content: Text('Vá para proxima pagina'),
+                              );
+                            });
+                      } else {
+                        //animate wrong input
+                        mPinController.notifyWrongInput();
+                      }
+                    },
+                  ),
+                  SizedBox(height: 32),
+                  GridView.count(
+                    crossAxisCount: 3,
+                    shrinkWrap: true,
+                    childAspectRatio: 1.6,
+                    children: List.generate(
+                        9, (index) => buildMaterialButton(index + 1)),
+                  ),
+                  GridView.count(
+                    crossAxisCount: 3,
+                    shrinkWrap: true,
+                    childAspectRatio: 1.6,
+                    children: [
+                      MaterialButton(
+                        onPressed: () {
+                          // mPinController.addInput('$input');
+                        },
+                        textColor: Colors.white,
+                        child: Icon(Icons.fingerprint),
+                      ),
+                      buildMaterialButton(0),
+                      MaterialButton(
+                        onPressed: () {
+                          mPinController.delete();
+                        },
+                        textColor: Colors.white,
+                        child: Icon(Icons.backspace_rounded),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  MaterialButton buildMaterialButton(int input) {
+    return MaterialButton(
+      onPressed: () {
+        mPinController.addInput('$input');
+      },
+      textColor: Colors.white,
+      child: Text(
+        '$input',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
